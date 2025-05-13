d0982485226b27f232371f104a93c8617a6ff4a6
yatishgautam00 <yatishgoutam00@gmail.com>
2025-05-13 18:04:31 +0530

    ASK ai link

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
               ) : isLoggedIn ? (
                 <Link
                   className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-                  href="/chat"
+                  href="/ask-ai"
                 >
                   Ask AI ➜
                 </Link>
